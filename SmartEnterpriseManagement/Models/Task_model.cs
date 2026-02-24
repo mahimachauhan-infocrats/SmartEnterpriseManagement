8415ebec7cba4cc48ea81547a4fc915ecc73997d
Khushhal Gupta <khushhal.gupta@infocratsweb.com>
2026-02-23 18:43:05 -0800

    just created a model of mu module

---
+++ SmartEnterpriseManagement/Models/Task_model.cs
@@ -0,0 +1,15 @@
+﻿using SmartEnterprise_Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartEnterpriseManagement.Models
+{
+    public class Task_model : BaseEntity
+    {
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
